5b0e890fe89dde538f11d306b66d9f81f490179b
sid-al <sendplusx@gmail.com>
2022-06-06 15:39:02 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Sid Ali';
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const upperCaseCurrentUser = currentUser.toUpperCase();
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+let shortGreeting = `Welcome, ${firstInitial}!`;
